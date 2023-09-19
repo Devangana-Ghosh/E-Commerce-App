3e7953b44b99e76c2d8fdfdda6cb7eae626fcd86
Admin <devangana1910@gmail.com>
2023-09-19 20:58:06 +0530

    theme updated

--- lib/presentation/Screen/theme.dart
+++ lib/presentation/Screen/theme.dart
@@ -3,22 +3,22 @@ import 'package:flutter/material.dart';
 class ThemeModel extends ChangeNotifier {
   ThemeData lightTheme = ThemeData.light().copyWith(
     backgroundColor: Colors.white,
-    // Add other light mode theme properties here
+
   );
 
   ThemeData darkTheme = ThemeData.dark().copyWith(
     backgroundColor: Colors.black12,
-    // Add other dark mode theme properties here
+
   );
 
   ThemeData currentTheme = ThemeData.light().copyWith(
     backgroundColor: Colors.white,
-    // Set the initial background color for light mode
+
   );
 
   void toggleTheme() {
     currentTheme =
-    currentTheme == lightTheme ? darkTheme : lightTheme; // Toggle between themes
+    currentTheme == lightTheme ? darkTheme : lightTheme;
     notifyListeners();
   }
 }
